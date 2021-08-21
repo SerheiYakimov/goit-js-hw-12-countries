96d26b3742cdc2c20308fb753338e284519c7970
SerheiYakimov <ysergey2901@gmail.com>
2021-08-22 00:04:50 +0300

    добавил lodash debounce

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 
 import contriesTemplate from './templates/contries-tps.hbs';
 import './sass/main.scss';
+
+
+// document.querySelector('input').addEventListener(
+//     'input',
+//     _.debounce(fetchCountries(searchQuery)), 500),
+//   );
