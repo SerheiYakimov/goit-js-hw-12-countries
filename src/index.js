1bb37683280c639150052b625e4a62ba338c4c86
SerheiYakimov <ysergey2901@gmail.com>
2021-08-25 18:13:24 +0300

    добавил pNofity

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@
 
 import debounce from 'lodash.debounce';
 import countryCardTps from './templates/countries-tps.hbs';
+import countriesListTps from './templates/countries-list-tps.hbs';
+ import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+
+import '@pnotify/core/dist/BrightTheme.css';
 // import fetchCountries from './js/fetchCountries.js';
 import './sass/main.scss';
+
+ alert({
+    text: 'Too many matches found! Please enter a more specific query!'
+  });
 
 
 const refs = {
@@ -25,10 +33,23 @@ fetch(`https://restcountries.eu/rest/v2/name/${name}`)
         return result.json();
     })
     .then(country => {
-        console.log(country);
-        const markUp = countryCardTps(country);
-        console.log(markUp);
-        refs.container.insertAdjacentHTML('afterbegin', markUp);
+        if (country.lenght >= 10) {
+            alert({
+    text: 'Too many matches found! Please enter a more specific query!'
+  });
+        }
+        if (country.lenght < 10 && country.lenght >= 2) {
+            const markUpCountries = countriesListTps(country);
+            
+            refs.container.insertAdjacentHTML('afterbegin', markUpCountries);
+        }
+        else {
+            const markUpCountry = countryCardTps(country);
+            
+            refs.container.insertAdjacentHTML('afterbegin', markUpCountry);
+        }
+        // console.log(country);
+        
        
     })
     .catch(error => {
